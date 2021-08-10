5b0949d178b7aac3bf48de43501f3cf73c35f25e
Tobias K <6317548+theCalcaholic@users.noreply.github.com>
2021-08-10 17:18:35 +0200

    Implement installer and launch script

---
+++ launch.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+
+latest_release="$(curl https://api.github.com/repos/Heroic-Games-Launcher/HeroicGamesLauncher/releases/latest)"
+download_url="$(grep -e "browser_download_url.*Heroic.*\.AppImage" "$latest_release" | cut -d : -f 2,3 | tr -d '" ,')"
+tag_name="$(grep \"tag_name\": "$latest_release" | cut -d ':' -f 2 | tr -d '" ,')"
+
+if ! [[ -f "./.version" ]] || [[ "$(cat ./.version)" != "$tag_name" ]]
+then
+  echo "Installing latest version of Heroic Games Launcher..."
+  wget -O ./Heroic-Launcher.AppImage "$download_url"
+  chmod +x ./Heroic-Launcher.AppImage
+fi
+
+nohup ./Heroic-Laucher.AppImage &
